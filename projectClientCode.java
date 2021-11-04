7655e45f8860c196e63327d9728c73f524aaf573
bogonissATWIT <bogoniss@wit.edu>
2021-11-04 12:24:19 -0400

    I hope that this works!

--- projectClientCode.java
+++ projectClientCode.java
@@ -3,6 +3,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class projectClientCode {
 	
@@ -45,7 +46,37 @@ public class projectClientCode {
 		
 		DataOutputStream outToServer = new DataOutputStream(connectionSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-		System.out.println("Client Running...");
+		
+		GameProject gameproject = new GameProject(outToServer,inFromServer);
+		Thread thread = new Thread(gameproject);
+		thread.start();
+		//optional formatting changes just to make it look a little nicer
+		
+		//I had the idea of basically taking the chatroom lab and adding to that and making the game. 
+		//This would make it look more professionally done and neat I guess. Having the names and all...
+		//Instead of "Name: 1 2" for like the row and columnm, we could have the server announce "Name played at position 1 2!"
+		//if that makes any sense... Obviously not going to stick you with all that just wanted to write it down so I knew what to do later
+		
+		
+		System.out.println("\t-= Welcome to TCP Tic-Tac-Toe! =-\nbefore you can play, please tell us your name: ");
+		Scanner s = new Scanner(System.in);
+		String name = s.nextLine();
+		outToServer.writeBytes(name + "\r\n");
+		
+		String play = s.nextLine();
+		
+		while(!play.toUpperCase().equals("QUIT")) {
+			
+			play = s.nextLine();
+			outToServer.writeBytes(play + "\r\n");
+		}
+		
+		s.close();
+		outToServer.close();
+		connectionSocket.close();
+		
+		
+		
 	}
 
 }
