223cb1ea78cd7668df80771b337e71ba3a011e60
Sam Bogonis <bogoniss@wit.edu>
2021-11-04 10:43:33 -0400

    Update projectClientCode.java
    testing to make sure egit pull works

--- projectClientCode.java
+++ projectClientCode.java
@@ -1,6 +1,10 @@
 
 public class projectClientCode {
-
+	
+	
+//testing to make sure that the pull works as expected with the new egit install
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
